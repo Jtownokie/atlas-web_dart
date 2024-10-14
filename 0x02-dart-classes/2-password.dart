14bc20c78670acecb06f16a4d7fd09926ba698a9
Jtownokie <christopher.stephens@atlasschool.com>
2024-10-14 12:47:45 -0500

    Completed task 2

---
+++ 0x02-dart-classes/2-password.dart
@@ -0,0 +1,19 @@
+class Password {
+  String? password;
+
+  Password({this.password});
+
+  bool isValid() {
+    if (this.password!.length >= 8 && this.password!.length <= 16) {
+      String pattern = r'^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9]).+$';
+      RegExp regExp = new RegExp(pattern);
+      return regExp.hasMatch(this.password!);
+    } else {
+      return false;
+    }
+  }
+
+  String toString() {
+    return 'Your Password is: ${this.password}';
+  }
+}
